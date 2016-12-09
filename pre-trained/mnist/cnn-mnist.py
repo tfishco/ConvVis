63a3fae5af5360508c3b1735b34d4043bb110ac9
Tom Fisher <thomasdenisfisher@gmail.com>
2016-12-09 06:38:03 +0000

    stuff

--- pre-trained/mnist/cnn-mnist.py
+++ pre-trained/mnist/cnn-mnist.py
@@ -62,12 +62,5 @@ saver = tf.train.Saver()
 sess.run(init_op)
 for i in range(2):
   batch = mnist.train.next_batch(50)
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict={
-        x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 saver.save(sess, "pre-trained/mnist/mnist.ckpt")
